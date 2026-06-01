701642a8807627392ec819e99bd7336b1c63ed41
Highimexy <teslawiktor@gmail.com>
2026-06-01 13:52:19 +0200

    feat: Basic footer

---
+++ frontend/app/components/footer.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+type FooterProps = {
+    className?: string;
+};
+
+export default function Footer({ className }: FooterProps) {
+    return (
+        <div className={`${className} gap-4`}>
+            <p className="font-serif text-2xl text-secondary font-bold">
+                JS(P)
+            </p>
+            <Link
+                target="_blank"
+                href={
+                    "https://helion.pl/ksiazki/javascript-przewodnik-poznaj-jezyk-mistrzow-programowania-wydanie-vii-david-flanagan,jspp7v.htm#format/d"
+                }
+                className="font-serif text-2xl text-secondary font-bold hover:underline"
+            >
+                Książka
+            </Link>
+        </div>
+    );
+}
